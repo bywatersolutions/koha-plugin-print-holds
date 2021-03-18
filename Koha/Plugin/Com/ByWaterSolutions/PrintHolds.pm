5e0dd81c816e3791bc891f3f351cca1edf24cbde
Kyle M Hall <kyle@bywatersolutions.com>
2021-03-18 10:35:11 -0400

    Initial commit

---
+++ Koha/Plugin/Com/ByWaterSolutions/PrintHolds.pm
@@ -0,0 +1,211 @@
+package Koha::Plugin::Com::ByWaterSolutions::PrintHolds;
+
+## It's good practice to use Modern::Perl
+use Modern::Perl;
+
+## Required for all plugins
+use base qw(Koha::Plugins::Base);
+
+## We will also need to include any Koha libraries we want to access
+use C4::Context;
+use C4::Auth;
+use Koha::Patron;
+use Koha::DateUtils;
+use Koha::Libraries;
+use Koha::Patron::Categories;
+use Koha::Account;
+use Koha::Account::Lines;
+use MARC::Record;
+use Cwd qw(abs_path);
+use Mojo::JSON qw(decode_json);;
+use URI::Escape qw(uri_unescape);
+use LWP::UserAgent;
+
+## Here we set our plugin version
+our $VERSION = "{VERSION}";
+our $MINIMUM_VERSION = "{MINIMUM_VERSION}";
+
+## Here is our metadata, some keys are required, some are optional
+our $metadata = {
+    name            => 'Auto-Print Holds',
+    author          => 'Kyle M Hall',
+    date_authored   => '2009-01-27',
+    date_updated    => "1900-01-01",
+    minimum_version => $MINIMUM_VERSION,
+    maximum_version => undef,
+    version         => $VERSION,
+    description     => 'Plugin to automatically print hold slips to printers as holds are placed',
+};
+
+## This is the minimum code required for a plugin's 'new' method
+## More can be added, but none should be removed
+sub new {
+    my ( $class, $args ) = @_;
+
+    ## We need to add our metadata here so our base class can access it
+    $args->{'metadata'} = $metadata;
+    $args->{'metadata'}->{'class'} = $class;
+
+    ## Here, we call the 'new' method for our base class
+    ## This runs some additional magic and checking
+    ## and returns our actual $self
+    my $self = $class->SUPER::new($args);
+
+    return $self;
+}
+
+## The existance of a 'report' subroutine means the plugin is capable
+## of running a report. This example report can output a list of patrons
+## either as HTML or as a CSV file. Technically, you could put all your code
+## in the report method, but that would be a really poor way to write code
+## for all but the simplest reports
+sub report {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    unless ( $cgi->param('output') ) {
+        $self->report_step1();
+    }
+    else {
+        $self->report_step2();
+    }
+}
+
+## The existance of a 'tool' subroutine means the plugin is capable
+## of running a tool. The difference between a tool and a report is
+## primarily semantic, but in general any plugin that modifies the
+## Koha database should be considered a tool
+sub tool {
+    my ( $self, $args ) = @_;
+
+    my $cgi = $self->{'cgi'};
+
+    unless ( $cgi->param('submitted') ) {
+        $self->tool_step1();
+    }
+    else {
+        $self->tool_step2();
+    }
+
+}
+
+## If your tool is complicated enough to needs it's own setting/configuration
+## you will want to add a 'configure' method to your plugin like so.
+## Here I am throwing all the logic into the 'configure' method, but it could
+## be split up like the 'report' method is.
+sub configure {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    unless ( $cgi->param('save') ) {
+        my $template = $self->get_template({ file => 'configure.tt' });
+
+        ## Grab the values we already have for our settings, if any exist
+        $template->param(
+            enable_opac_payments => $self->retrieve_data('enable_opac_payments'),
+            foo             => $self->retrieve_data('foo'),
+            bar             => $self->retrieve_data('bar'),
+            last_upgraded   => $self->retrieve_data('last_upgraded'),
+        );
+
+        $self->output_html( $template->output() );
+    }
+    else {
+        $self->store_data(
+            {
+                enable_opac_payments => $cgi->param('enable_opac_payments'),
+                foo                => $cgi->param('foo'),
+                bar                => $cgi->param('bar'),
+                last_configured_by => C4::Context->userenv->{'number'},
+            }
+        );
+        $self->go_home();
+    }
+}
+
+## This is the 'install' method. Any database tables or other setup that should
+## be done when the plugin if first installed should be executed in this method.
+## The installation method should always return true if the installation succeeded
+## or false if it failed.
+sub install() {
+    my ( $self, $args ) = @_;
+
+    my $table = $self->get_qualified_table_name('mytable');
+
+    return 1;
+}
+
+## This is the 'upgrade' method. It will be triggered when a newer version of a
+## plugin is installed over an existing older version of a plugin
+sub upgrade {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+## This method will be run just before the plugin files are deleted
+## when a plugin is uninstalled. It is good practice to clean up
+## after ourselves!
+sub uninstall() {
+    my ( $self, $args ) = @_;
+
+    my $table = $self->get_qualified_table_name('mytable');
+
+    return 1;
+}
+
+## These are helper functions that are specific to this plugin
+## You can manage the control flow of your plugin any
+## way you wish, but I find this is a good approach
+sub report_step1 {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    my $template = $self->get_template({ file => 'report-step1.tt' });
+
+    $self->output_html( $template->output() );
+}
+
+sub report_step2 {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    my $dbh = C4::Context->dbh;
+
+    my $template = $self->get_template({ file => $filename });
+
+    print $template->output();
+}
+
+sub tool_step1 {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    my $template = $self->get_template({ file => 'tool-step1.tt' });
+
+    $self->output_html( $template->output() );
+}
+
+sub tool_step2 {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    my $template = $self->get_template({ file => 'tool-step2.tt' });
+
+    $self->output_html( $template->output() );
+}
+
+=head3 cronjob_nightly
+
+Plugin hook running code from a cron job
+
+=cut
+
+sub cronjob_nightly {
+    my ( $self ) = @_;
+
+    print "Remember to clean the kitchen\n";
+}
+
+
+1;
